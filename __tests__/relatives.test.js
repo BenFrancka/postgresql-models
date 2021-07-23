0fb380fac78406891afcfcb80906e6901f721bcf
BenFrancka <benfrancka@gmail.com>
2021-07-23 10:29:58 -0700

    adds put route to relatives.js, test is now passing

--- __tests__/relatives.test.js
+++ __tests__/relatives.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Relative from '../lib/models/Relative.js';
+//import Relative from '../lib/models/Relative.js';
 
 describe('relatives routes', () => {
   beforeEach(() => {
