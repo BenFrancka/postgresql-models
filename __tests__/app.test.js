08811720e2545f6ef98c5f0355fdcd1cf93b2cf8
BenFrancka <benfrancka@gmail.com>
2021-07-22 17:42:10 -0700

    test for PUT route for dinosaurs passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,27 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+//import Dinosaur from '../lib/models/Dinosaur.js';
 
-describe('demo routes', () => {
+describe('dinosaur routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-});
+  it('creates a dinosaur using POST', async () => {
+    const utahRaptor = {
+      species: 'utahraptor',
+      diet: 'carnivore',
+      timePeriod: 'cretaceous'
+    };
+    const res = await request(app)
+      .post('/api/v1/dinosaurs')
+      .send(utahRaptor);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...utahRaptor
+    });
+  });
+
+
+}); 
