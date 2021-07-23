5521b71abc40c50b7feee46dad9ba2dfdf54e20f
BenFrancka <benfrancka@gmail.com>
2021-07-23 10:03:37 -0700

    adds relatives table to setup.sql, adds relatives.test.js file, relatives.js to controllers, Relative.js to models, and updatesapp.js with relatives path

---
+++ __tests__/relatives.test.js
@@ -0,0 +1,16 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import Relative from '../lib/models/Relative.js';
+
+describe('relatives routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  
+
+
+
+});
