093858507d9f9dcf83cd784e5af75d0f9c3aa56f
BenFrancka <benfrancka@gmail.com>
2021-07-23 11:59:32 -0700

    adds initial test for POST to beers.test.js

--- __tests__/beers.test.js
+++ __tests__/beers.test.js
@@ -4,9 +4,19 @@ import request from 'supertest';
 import app from '../lib/app.js';
 //import Beer from '../lib/models/Beer.js';
 
-describe('dinosaur routes', () => {
+describe('beer routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a beer with POST', async () => {
+    const pilsnerUrquell = { beerName: 'pilsner urquell', nationality: 'czech republic', variety: 'pilsner', alcoholPercentage: 4 };
+
+    const res = await request(app)
+      .post('/api/v1/beers')
+      .send(pilsnerUrquell);
+    
+    expect(res.body).toEqual(pilsnerUrquell);
   });
 
 
