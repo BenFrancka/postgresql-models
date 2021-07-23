3c5340c322c4e34cc26e2ccdf6c8a2edc9f2e4f8
BenFrancka <benfrancka@gmail.com>
2021-07-23 15:06:15 -0700

    adds initial test for posting a book to books.test.js

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,13 +2,25 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Book from '../lib/models/Book.js';
+//import Book from '../lib/models/Book.js';
 
 
 describe('beer routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  
+
+  it('creates a book with POST', async () => {
+    const neuromancer = { title: 'neuromancer', authorName: 'william gibson', pageCount: 271 };
+
+    const res = await request(app)
+      .post('/api/v1/books')
+      .send(neuromancer);
+    
+    expect(res.body).toEqual({
+      id: '1',
+      ...neuromancer
+    });
+  });
 
 });
