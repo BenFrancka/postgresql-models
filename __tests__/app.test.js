3ed6c8576d39dbb4fb40d04839cc7277d9e96c45
BenFrancka <benfrancka@gmail.com>
2021-07-22 17:48:30 -0700

    adds initial test fr get by id for dinosaurs to app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-//import Dinosaur from '../lib/models/Dinosaur.js';
+import Dinosaur from '../lib/models/Dinosaur.js';
 
 describe('dinosaur routes', () => {
   beforeEach(() => {
@@ -22,6 +22,19 @@ describe('dinosaur routes', () => {
       id: '1',
       ...utahRaptor
     });
+  });
+
+  it('gets a dinosaur by id using GET', async () => {
+    const utahRaptor = await Dinosaur.insert({
+      species: 'utahraptor',
+      diet: 'carnivore',
+      timePeriod: 'cretaceous'
+    });
+    const res = await request(app)
+      .get(`/api/v1/dinosaurs/${utahRaptor.id}`);
+      
+
+    expect(res.body).toEqual(utahRaptor);
   });
 
 
