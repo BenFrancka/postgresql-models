cecf3fc41aedb1e0e5ba9079a9aa6198ac37146e
BenFrancka <benfrancka@gmail.com>
2021-07-23 11:00:49 -0700

    adds initial test for get all relatives to relatives.test.js

--- __tests__/relatives.test.js
+++ __tests__/relatives.test.js
@@ -37,6 +37,33 @@ describe('relatives routes', () => {
     expect(res.body).toEqual(ruth);
   });
 
+  it('gets all relatives with GET', async () => {
+    const ruth = await Relative.insert({
+      firstName: 'ruth',
+      relation: 'grandmother',
+      numberOfSiblings: 3,
+      age: 94
+    });
+    const martha = await Relative.insert({
+      firstName: 'martha',
+      relation: 'aunt',
+      numberOfSiblings: 4,
+      age: 48
+    });
+    const sarah = await Relative.insert({
+      firstName: 'sarah',
+      relation: 'sister',
+      numberOfSiblings: 1,
+      age: 31
+    });
+
+    const res = await request(app)
+      .get('/api/v1/relatives/');
+      
+
+    expect(res.body).toEqual([ruth, martha, sarah]);
+  });
+
 
 
 
