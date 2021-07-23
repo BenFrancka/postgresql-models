d00ba95e1acc06df38766e74488f7ae3da92128e
BenFrancka <benfrancka@gmail.com>
2021-07-23 09:50:17 -0700

    adds relatives table to setup.sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -6,3 +6,13 @@ CREATE TABLE dinosaurs (
     diet TEXT NOT NULL,
     time_period TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS relatives;
+
+CREATE TABLE relatives (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    relation TEXT NOT NULL,
+    number_of_siblings INTEGER NOT NULL,
+    age INTEGER NOT NULL
+);
